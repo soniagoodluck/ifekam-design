49bc123fa42e28e6338251b795da9115be7c1f28
Sonia <soniagoodluck@yahoo.co.uk>
2022-03-15 12:26:49 +0000

    created homepage

---
+++ src/homePage.component.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <div className='directory-menu'>
+          <div className='menu-item'>
+              <div className='content'>
+                  <h1 className= 'title'> CHEVEUX</h1>
+                  <span className= 'subtitle'> ACHETER MAINTENET</span>
+              </div>
+
+         </div>
+         <div className='menu-item'>
+              <div className='content'>
+                  <h1 className= 'title'> CH</h1>
+                  <span className= 'subtitle'> ACHETER MAINTENET</span>
+              </div>
+
+         </div>
+         <div className='menu-item'>
+              <div className='content'>
+                  <h1 className= 'title'> ASS</h1>
+                  <span className= 'subtitle'> ACHETER MAINTENET</span>
+              </div>
+
+         </div>
+         <div className='menu-item'>
+              <div className='content'>
+                  <h1 className= 'title'> FEMME</h1>
+                  <span className= 'subtitle'> ACHETER MAINTENET</span>
+              </div>
+
+         </div>
+         <div className='menu-item'>
+              <div className='content'>
+                  <h1 className= 'title'> HOMME</h1>
+                  <span className= 'subtitle'> ACHETER MAINTENET</span>
+              </div>
+
+         </div>
+         <div className='menu-item'>
+              <div className='content'>
+                  <h1 className= 'title'> HAIRS</h1>
+                  <span className= 'subtitle'> ACHETER MAINTENET</span>
+              </div>
+
+         </div>
+        </div>
+    </div>
+);
+
+export default HomePage;
